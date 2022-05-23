f88df7afa591f33ed66461fd4152521c3c4d201a
Onkar Deshpande <onkar.deshpande@rubrik.com>
2022-05-23 21:06:46 +0530

    Add husky and hooks

--- pages/api/__tests__/echo.test.ts
+++ pages/api/__tests__/echo.test.ts
@@ -1,47 +1,44 @@
-import { createMocks, RequestMethod } from "node-mocks-http";
-import type { NextApiRequest, NextApiResponse } from "next";
-import echoHandler, { Data } from "../echo";
+import { createMocks, RequestMethod } from 'node-mocks-http'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import echoHandler, { Data } from '../echo'
 
-describe("Echo API", () => {
-  const data = {
-    "string query key": "string query value",
-    "array query key": "array query value",
-  };
-  it("should echo GET requests correctly", async () => {
-    const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
-      createMocks({ method: "GET" });
-    req.query = data;
-    await echoHandler(req, res);
+describe('Echo API', () => {
+    const data = {
+        'string query key': 'string query value',
+        'array query key': 'array query value',
+    }
+    it('should echo GET requests correctly', async () => {
+        const { req, res } = createMocks({ method: 'GET' })
+        req.query = data
+        await echoHandler(req, res)
 
-    expect(res.statusCode).toBe(200);
-    expect(res.statusMessage).toEqual("OK");
-    expect(res._getJSONData()).toStrictEqual({
-      name: "ECHO GET",
-      get: data,
-    } as Data);
-  });
-  it("should echo POST requests correctly", async () => {
-    const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
-      createMocks({ method: "POST" });
-    req.body = data;
-    await echoHandler(req, res);
+        expect(res.statusCode).toBe(200)
+        expect(res.statusMessage).toEqual('OK')
+        expect(res._getJSONData()).toStrictEqual({
+            name: 'ECHO GET',
+            get: data,
+        } as Data)
+    })
+    it('should echo POST requests correctly', async () => {
+        const { req, res } = createMocks({ method: 'POST' })
+        req.body = data
+        await echoHandler(req, res)
 
-    expect(res.statusCode).toBe(200);
-    expect(res.statusMessage).toEqual("OK");
-    expect(res._getJSONData()).toStrictEqual({
-      name: "ECHO POST",
-      post: data,
-    } as Data);
-  });
-  it("should echo PUT requests correctly", async () => {
-    const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
-      createMocks({ method: "PUT" });
-    await echoHandler(req, res);
+        expect(res.statusCode).toBe(200)
+        expect(res.statusMessage).toEqual('OK')
+        expect(res._getJSONData()).toStrictEqual({
+            name: 'ECHO POST',
+            post: data,
+        } as Data)
+    })
+    it('should echo PUT requests correctly', async () => {
+        const { req, res } = createMocks({ method: 'PUT' })
+        await echoHandler(req, res)
 
-    expect(res.statusCode).toBe(200);
-    expect(res.statusMessage).toEqual("OK");
-    expect(res._getJSONData()).toStrictEqual({
-      name: "ECHO PUT",
-    } as Data);
-  });
-});
+        expect(res.statusCode).toBe(200)
+        expect(res.statusMessage).toEqual('OK')
+        expect(res._getJSONData()).toStrictEqual({
+            name: 'ECHO PUT',
+        } as Data)
+    })
+})
--- pages/api/echo.ts
+++ pages/api/echo.ts
@@ -1,31 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export type Data = {
-  get?: {
-    [key: string]: string | string[];
-  };
-  post?: {
-    [key: string]: string | string[];
-  };
-  name: string;
-};
+    get?: {
+        [key: string]: string | string[]
+    }
+    post?: {
+        [key: string]: string | string[]
+    }
+    name: string
+}
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  switch (req.method) {
-    case "GET":
-      res.status(200).json({ name: "ECHO GET", get: req.query });
-      break;
+    switch (req.method) {
+        case 'GET':
+            res.status(200).json({ name: 'ECHO GET', get: req.query })
+            break
 
-    case "POST":
-      res.status(200).json({ name: "ECHO POST", post: req.body });
-      break;
+        case 'POST':
+            res.status(200).json({ name: 'ECHO POST', post: req.body })
+            break
 
-    default:
-      res.status(200).json({ name: `ECHO ${req.method}` });
-      break;
-  }
+        default:
+            res.status(200).json({ name: `ECHO ${req.method}` })
+            break
+    }
 }
