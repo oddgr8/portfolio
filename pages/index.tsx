52472fd6c210efcb5e85e6aea5d1784de9007d3c
Onkar Deshpande <onkar.deshpande@rubrik.com>
2022-06-29 00:22:03 +0530

    Add AboutMe section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
 import type { NextPage } from 'next'
 
+function AboutMe() {
+    return (
+        <div
+            className="hero rounded-lg min-h-[50vh]"
+            style={{ backgroundImage: 'url(/bg_me.jpg)' }}
+        >
+            <div className="hero-overlay bg-opacity-60 rounded-lg flex flex-col place-content-center place-items-center text-neutral-content text-center">
+                <div className="max-w-md">
+                    <h1 className="m-4 text-5xl font-bold">Onkar Deshpande</h1>
+                    <p className="m-2 text-lg">
+                        Hi, welcome to my corner of the internet!
+                    </p>
+                    <p className="m-2">
+                        I{"'"}m a Software engineer passionate about learning
+                        new things and trying out new technologies. I{"'"}m
+                        currently into in blockchain and distributed systems.
+                    </p>
+                    <p className="m-2">
+                        I love reading Physics, Math and a bit of Philosophy and
+                        I{"'"}m always up for interesting conversations,
+                        regardless of the topic.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const Home: NextPage = () => {
-    return <div>Onkar Deshpande</div>
+    return (
+        <div className="flex flex-col items-center">
+            <AboutMe />
+        </div>
+    )
 }
 
 export default Home
